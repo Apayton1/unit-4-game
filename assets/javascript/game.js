6595388ee9a44cbc1c09f3b19962773a7b2737f7
Aquila Payton <44341215+Apayton1@users.noreply.github.com>
2019-06-10 23:07:57 -0400

    I added the random number generators and the on click function and used 'THIS' in order to avoid having to create for separate on click functions. I also added a ne class to each button and changed the class names for the text area for wins, losses, total, and target. Within the on click function I added conditions which arent yet completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,68 @@
 // RANDOM NUMBER NEEDS TO BE GENERATED AT THE BEGINNING OF THE GAME BETWEEN 19 AND 120
 
-$(document).ready(function() {
-    var Random = Math.floor((Math.random()* 120) + 1);
-    document.getElementById("targetNumber").innerHTML= Random;
-}) 
+$(document).ready(function () {
 
-    $("targetNumber").text(Random);
-    
-
-// EACH CHARACTER HAS TO HAVE A RANDOM NUMBER
-    var number1 = Math.floor(Math.random()*12 + 1);
-    var number2 = Math.floor(Math.random()*12 + 1);
-    var number3 = Math.floor(Math.random()*12 + 1);
-    var number4 = Math.floor(Math.random()*12 + 1);
-
-// CALCULATIONS FOR TOTAL NUMBER, WINS, AND LOSSES
-    var totalScore = 0;
+    var target = 0;
+    var total = 0;
     var wins = 0;
     var losses = 0;
+
+        var targetNumber = Math.floor(Math.random() * (102)) + 19;
+
+
+    // function targetNumber (min,max) {
+    //     return Math.floor(Math.random() * (max - min + 1)) + min;
+        
+    //     console.log(targetNumber(19,120));
+    // }
+
+    
+    $("#targetNumberText").text(targetNumber);
+
+
+    // $(document).ready(function() {
+    //     var Random = Math.floor((Math.random()* 120) + 1);
+    //     document.getElementById("targetNumber").innerHTML= Random;
+    // }) 
+
+    //     $("targetNumber").text(Random);
+
+
+    // EACH CHARACTER HAS TO HAVE A RANDOM NUMBER FROM 0 TO 12
+    var number1 = Math.floor(Math.random() * 13);
+    // $(".yourTotalText").text(number1);
+    var number2 = Math.floor(Math.random() * 13);
+    var number3 = Math.floor(Math.random() * 13);
+    var number4 = Math.floor(Math.random() * 13);
+
+    $("#garfield1").attr("data-value",number1);
+    $("#blue2").attr("data-value",number2);
+    $("#catdog3").attr("data-value",number3);
+    $("#scoobydoo4").attr("data-value",number4);
+
+    
+    $(".click").on("click",function(){
+        console.log($(this).attr("data-value"));
+        total = total + parseInt ($(this).attr("data-value"));
+        $(".yourTotalText").text(total);
+
+        if(total === target){
+            wins++;
+            $(".winsText").text(wins);
+            
+        }
+        else if(total > target){
+            losses--;
+            $(".lossesText").text(losses);
+        }
+    })
+
+    
+
+
+    // // CALCULATIONS FOR TOTAL NUMBER, WINS, AND LOSSES
+    // var totalScore = 0;
+    // var wins = 0;
+    // var losses = 0;
+
+})
