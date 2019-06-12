f7e6ae45e387461c1e6c307fc94a3230f264a4dd
Aquila Payton <44341215+Apayton1@users.noreply.github.com>
2019-06-12 18:47:35 -0400

    I deleted the target variable and used target number variable instead in if else statement.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,21 +2,21 @@
 
 $(document).ready(function () {
 
-    var target = 0;
+
     var total = 0;
     var wins = 0;
     var losses = 0;
 
-        var targetNumber = Math.floor(Math.random() * (102)) + 19;
+    var targetNumber = Math.floor(Math.random() * (102)) + 19;
 
 
     // function targetNumber (min,max) {
     //     return Math.floor(Math.random() * (max - min + 1)) + min;
-        
+
     //     console.log(targetNumber(19,120));
     // }
 
-    
+
     $("#targetNumberText").text(targetNumber);
 
 
@@ -29,35 +29,69 @@ $(document).ready(function () {
 
 
     // EACH CHARACTER HAS TO HAVE A RANDOM NUMBER FROM 0 TO 12
-    var number1 = Math.floor(Math.random() * 13);
+
+
+    var number1 = Math.floor(Math.random() * 13) + 1;
     // $(".yourTotalText").text(number1);
-    var number2 = Math.floor(Math.random() * 13);
-    var number3 = Math.floor(Math.random() * 13);
-    var number4 = Math.floor(Math.random() * 13);
+    var number2 = Math.floor(Math.random() * 13) + 1;
+    var number3 = Math.floor(Math.random() * 13) + 1;
+    var number4 = Math.floor(Math.random() * 13) + 1;
 
-    $("#garfield1").attr("data-value",number1);
-    $("#blue2").attr("data-value",number2);
-    $("#catdog3").attr("data-value",number3);
-    $("#scoobydoo4").attr("data-value",number4);
 
-    
-    $(".click").on("click",function(){
+    // I TRIED STORING THE NUMBER VARIABLES IN A FUNCTION SO THAT I COULD AVOID HAVING TO LIST EACH
+    // VARIABLE IN THE IF ELSE STATEMENT.
+    // var charNumberReset = function () {
+    //     number1 = Math.floor(Math.random() * 13) + 1;
+    //     // $(".yourTotalText").text(number1);
+    //     number2 = Math.floor(Math.random() * 13) + 1;
+    //     number3 = Math.floor(Math.random() * 13) + 1;
+    //     number4 = Math.floor(Math.random() * 13) + 1;
+    // };
+
+
+    $("#garfield1").attr("data-value", number1);
+    $("#blue2").attr("data-value", number2);
+    $("#catdog3").attr("data-value", number3);
+    $("#scoobydoo4").attr("data-value", number4);
+
+
+    $(".click").on("click", function () {
         console.log($(this).attr("data-value"));
-        total = total + parseInt ($(this).attr("data-value"));
+        total = total + parseInt($(this).attr("data-value"));
         $(".yourTotalText").text(total);
 
-        if(total === target){
+        if (total === targetNumber) {
             wins++;
             $(".winsText").text(wins);
-            
+            total = 0;
+            // charNumberReset();
+            // number1;
+            // // $(".yourTotalText").text(number1);
+            // number2;
+            // number3;
+            // number4;
+            targetNumber;
+
+
+
         }
-        else if(total > target){
-            losses--;
+        else if (total > targetNumber) {
+            losses++;
             $(".lossesText").text(losses);
+            total = 0;
+            // charNumberReset();
+            // number1 = Math.floor(Math.random() * 13) + 1;
+            // // $(".yourTotalText").text(number1);
+            // number2 = Math.floor(Math.random() * 13) + 1;
+            // number3 = Math.floor(Math.random() * 13) + 1;
+            // number4 = Math.floor(Math.random() * 13) + 1;
+            targetNumber;
+
+
         }
     })
 
-    
+
 
 
     // // CALCULATIONS FOR TOTAL NUMBER, WINS, AND LOSSES
